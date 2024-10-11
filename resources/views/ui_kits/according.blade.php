c4f0406dc5fb3201205b0053364a2f0662f94d36
kirillio25 <this.kirill.ionkin@gmail.com>
2024-10-11 17:11:57 +0500

    НАчал делать аккардион

--- resources/views/ui_kits/according.blade.php
+++ resources/views/ui_kits/according.blade.php
@@ -52,16 +52,81 @@
                             </div>
                             <div class="collapse show" id="collapseFour" aria-labelledby="headingOne"
                                 data-bs-parent="#accordion1">
-                                <div class="card-body">Anim pariatur cliche reprehenderit, enim eiusmod high life
-                                    accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat
-                                    skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf
-                                    moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda
-                                    shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred
-                                    nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings
-                                    occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably
-                                    haven't heard of them accusamus labore sustainable VHS.</div>
+                                <div class="file-content">
+                                    <div class="card">
+
+                                        <div class="card-body file-manager">
+                                            <h5>Материалы</h5>
+                                            <ul class="quick-file d-flex flex-row">
+                                                <li>
+                                                    <div class="quick-box"><i class="fa fa-youtube-play font-danger"></i></div>
+                                                    <h6>Videos</h6>
+                                                </li>
+                                                <li>
+                                                    <div class="quick-box"><i class="fa fa-youtube-play font-danger"></i></div>
+                                                    <h6>Videos</h6>
+                                                </li>
+                                                <li>
+                                                    <div class="quick-box"><i class="fa fa-youtube-play font-danger"></i></div>
+                                                    <h6>Videos</h6>
+                                                </li>
+                                                <li>
+                                                    <div class="quick-box"><i class="fa fa-youtube-play font-danger"></i></div>
+                                                    <h6>Videos</h6>
+                                                </li>
+                                                <li>
+                                                    <div class="quick-box"><i class="fa fa-file-archive-o font-secondary"></i></div>
+                                                    <h6>Zip File</h6>
+                                                </li>
+                                                <li>
+                                                    <div class="quick-box"><i class="fa fa-trash font-danger"></i></div>
+                                                    <h6>Trash</h6>
+                                                </li>
+                                            </ul>
+                                            <h5 class="mt-4">Домашняя работа</h5>
+                                            <ul class="folder">
+                                                <li class="folder-box">
+                                                    <div class="d-block"><i class="f-44 fa fa-file-archive-o txt-warning"></i><i
+                                                            class="fa fa-ellipsis-v me-0 f-14 ellips"></i>
+                                                        <div class="mt-3">
+                                                            <h6>Tivo admin</h6>
+                                                            <p>20 file<span class="pull-right"> <i class="fa fa-clock-o"> </i> 2 Hour
+                                                                    ago</span></p>
+                                                        </div>
+                                                    </div>
+                                                </li>
+                                            </ul>
+
+                                            <!-- Не забыть -->
+                                            <div class="d-none" id="uploaded-file">
+                                                <h5 class="mt-4">Загруженный файл </h5>
+                                                <ul class="d-flex flex-row files-content">
+                                                    <li class="folder-box d-flex align-items-center">
+                                                        <div class="d-flex align-items-center files-list">
+                                                            <div class="flex-shrink-0 file-left"><i
+                                                                    class="f-22 fa fa-file-archive-o font-warning"></i></div>
+                                                            <div class="flex-grow-1 ms-3">
+                                                                <h6>Project.zip</h6>
+                                                                <p>1 Day ago, 1.9 GB</p>
+                                                            </div>
+                                                        </div>
+                                                    </li>
+                                                </ul>
+                                            </div>
+                                        </div>
+
+         
+                              
+                                    </div>
+                                    <div class="btn btn-outline-primary ms-2">
+                                            <i data-feather="upload"> </i>Загрузить работу
+                                        </div>
+                                </div>
+
                             </div>
+                            
                         </div>
+                        
                         <div class="card">
                             <div class="card-header bg-primary" id="headingFive">
                                 <h5 class="mb-0">
@@ -123,4 +188,12 @@
 <script src="{{ asset('assets/js/counter/counter-custom.js') }}"></script>
 <script src="{{ asset('assets/js/custom-card/custom-card.js') }}"></script>
 <script src="{{ asset('assets/js/chart-widget.js') }}"></script>
+
+<!-- Видео -->
+<script src="{{ asset('assets/js/icons/feather-icon/feather-icon-clipart.js') }}"></script>
+<script src="{{ asset('assets/js/typeahead/handlebars.js') }}"></script>
+<script src="{{ asset('assets/js/typeahead/typeahead.bundle.js') }}"></script>
+<script src="{{ asset('assets/js/typeahead/typeahead.custom.js') }}"></script>
+<script src="{{ asset('assets/js/typeahead-search/handlebars.js') }}"></script>
+<script src="{{ asset('assets/js/typeahead-search/typeahead-custom.js') }}"></script>
 @endsection
